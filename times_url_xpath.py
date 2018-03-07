4c5e6d95dd9f15397afce9e909c33f9bb5266187
denyszamiatin <denys.zamiatin@gmail.com>
2018-03-07 19:20:57 +0200

    Code-review #2

--- times_url_xpath.py
+++ times_url_xpath.py
@@ -1,18 +1,19 @@
 import re
 import configparser
 
+URL_PATTERN = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
 
-def extract_urls(fname):
-    with open(fname) as f:
-        return re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', f.read())
+def read_file(filename):
+    with open(filename) as f:
+        return f.read()
 
 
+def extract_urls(text):
+    return re.findall(URL_PATTERN, text)
 
-start_urls = extract_urls("links.txt")
 
-
-
+start_urls = extract_urls(read_file("links.txt"))
 
 config = configparser.ConfigParser()
 config.read('xpath_query.ini')
