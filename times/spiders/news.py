348dec9f97e058d19e2202b346e3399deb5c9790
gordon <dimagordon@gmail.com>
2018-03-20 20:13:59 +0200

    added new spider for 24tv.ua

--- times/spiders/news.py
+++ times/spiders/news.py
@@ -1,36 +1,22 @@
 import scrapy
 from scrapy import Request
-import configparser
+
+from times.tools.config import Config
 
 
-class Config:
-    def __init__(self):
-        self.config = configparser.ConfigParser()
-        self.config.read('xpath_query.ini')
-        self.newssite = self.config['NewsSite']
-        self.xpath = self.config['XPaths']
-
-
-config = Config()
-
-
-class TimesSpider(scrapy.Spider):
-    name = 'news'
-    allowed_domains = [config.newssite['Domain']]
-    start_urls = [config.newssite['StartURL']]
+class BaseSpider(scrapy.Spider):
 
     def parse(self, response):
-        articles = response.xpath(config.xpath['Articles'])
-
+        articles = response.xpath(self.config.newssite['Articles'])
         for article in articles:
-            title = article.xpath(config.xpath['Title']).extract_first("")
-            relative_url = article.xpath(config.xpath['URL']).extract_first()
+            title = article.xpath(self.config.newssite['Title']).extract_first()
+            relative_url = article.xpath(self.config.newssite['URL']).extract_first()
             absolute_url = response.urljoin(relative_url)
             yield Request(absolute_url,
                           callback=self.parse_article,
                           meta={'URL': absolute_url, 'Title': title.rstrip()})
 
-        relative_next_url = response.xpath(config.xpath['NextURL']).extract_first()
+        relative_next_url = response.xpath(self.config.newssite['NextURL']).extract_first()
         absolute_next_url = response.urljoin(relative_next_url)
         yield Request(absolute_next_url,
                       callback=self.parse)
@@ -38,8 +24,29 @@ class TimesSpider(scrapy.Spider):
     def parse_article(self, response):
         url = response.meta.get('URL')
         title = response.meta.get('Title')
-        text = "".join(line for line in response.xpath(config.xpath['Text']).extract())
-        author = response.xpath(config.xpath['Author']).extract()
-        date = response.xpath(config.xpath['Date']).extract()
+        text = "".join(response.xpath(self.config.newssite['Text']).extract())
+        author = response.xpath(self.config.newssite['Author']).extract_first()
+        date = self.get_date(response)
         yield {'Title': title.strip(), 'Date': date, 'Author': author, 'URL': url, 'Text': text.strip()}
+
+    def get_date(self, response):
+        return response.xpath(self.config.newssite['Date']).extract_first()
+
+
+class CensorNet(BaseSpider):
+    name = 'CensorNet'
+    config = Config(name)
+    allowed_domains = [config.newssite['Domain']]
+    start_urls = [config.newssite['StartURL']]
+
+
+class News24TVSpider(BaseSpider):
+    name = 'News24TV'
+    config = Config(name)
+    allowed_domains = [config.newssite['Domain']]
+    start_urls = [config.newssite['StartURL']]
+
+    def get_date(self, response):
+        return response.xpath(self.config.newssite['Date']).re_first(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}')
+
 
