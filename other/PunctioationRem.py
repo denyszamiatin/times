5821cf5b58fff9ee637366bdaaf2368451465684
gordon <dimagordon@gmail.com>
2018-03-21 15:49:19 +0200

    change tab indents to spaces. added self to method args ensure pep8

--- other/PunctioationRem.py
+++ other/PunctioationRem.py
@@ -1,16 +1,12 @@
 import string
 
+
 class PunctuationRem:
 
-	def read_article():
-		with open('article.txt','r') as f:
-			text = f.read()
-		words = text.split()
-		table =  str.maketrans('', '', string.punctuation)
-		stripped = [w.translate(table) for w in words]
-		return stripped
-	
-
-
-
-
+    def read_article(self):
+        with open('article.txt', 'r') as f:
+            text = f.read()
+            words = text.split()
+            table = str.maketrans('', '', string.punctuation)
+            stripped = [w.translate(table) for w in words]
+        return stripped
