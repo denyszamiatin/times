349e3c1b857e1dd62d9bf3bf44ffda58d47845c7
gordon <dimagordon@gmail.com>
2018-03-21 16:25:03 +0200

    fixed bug with processing multiple spiders

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
+import logging
+
 from scrapy.crawler import CrawlerProcess
-from times.spiders.news import News24TVSpider, CensorNet
+from scrapy.utils.project import get_project_settings
+
+from times.spiders import News24TVSpider, CensorNet
 
 SPIDERS = (
     CensorNet,
     News24TVSpider,
 )
 
+logging.getLogger('scrapy').propagate = False
 
-process = CrawlerProcess({'USER_AGENT': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0'})
-process.crawl(*SPIDERS)
+process = CrawlerProcess(get_project_settings())
+for spider in SPIDERS:
+    process.crawl(spider)
 process.start()
