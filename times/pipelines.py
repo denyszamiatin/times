699c83423fd4e5481d73727e6a22d1389e6cd081
gordon <dimagordon@gmail.com>
2018-03-21 16:32:09 +0200

    created pipeline for csv feed

--- times/pipelines.py
+++ times/pipelines.py
@@ -1,11 +1,36 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+import time
+
+from scrapy.exporters import CsvItemExporter
 
 
-class TimesPipeline(object):
+def _generate_filename(spider, date_format="%Y-%m-%d_%H:%M:%S"):
+    _prepare_file_folders(spider)
+    time_appendix = time.strftime(date_format)
+    return spider.settings.get("FEED_URI_CSV") % ({"name": spider.name, "time": time_appendix})
+
+
+def _prepare_file_folders(spider):
+    if not os.path.exists('data'):
+        os.mkdir('data')
+    if not os.path.exists('data/%s' % spider.name):
+        os.mkdir('data/%s' % spider.name)
+
+
+class CsvWriterPipeline(object):
+
+    def open_spider(self, spider):
+        filename = _generate_filename(spider)
+        self.file = open(filename, 'wb')
+        self.exporter = CsvItemExporter(self.file)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
