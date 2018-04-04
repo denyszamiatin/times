d6a07559a821d49aa152a419edd7a05288d6ccff
Dmitriy Gordon <gordonworkspace@gmail.com>
2018-04-04 17:24:48 +0300

    removed download delay and scrapy pipelines

--- times/settings.py
+++ times/settings.py
@@ -33,7 +33,7 @@ FEED_URI_CSV = 'data/%(name)s/%(time)s.csv'
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 2
+DOWNLOAD_DELAY = 0
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -70,9 +70,9 @@ DOWNLOAD_DELAY = 2
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   'times.pipelines.CsvWriterPipeline': 999,
-}
+# ITEM_PIPELINES = {
+#    'times.pipelines.CsvWriterPipeline': 999,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
